9a28fffb4446586a85c5c9b00b4ce337080aebc5
Serap BALCI <81075168+reset-34@users.noreply.github.com>
2024-08-01 16:03:18 +0300

    Add project files.

---
+++ A’dan Z’ye kadar ekrana yazan/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_dan_Z_ye_kadar_ekrana_yazan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           for (char i= 'a'; i<= 'z'; i++ )
+
+
+
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
